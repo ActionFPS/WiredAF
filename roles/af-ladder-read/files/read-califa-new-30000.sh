43ab2af0c721d2cf899f615425e8a915bd895f20
Vykintas Narmontas (William) <william@scalawilliam.com>
2017-04-25 20:28:07 +0800

    Looks like the name changed a little bit

--- roles/af-ladder-read/files/read-califa-new-30000.sh
+++ roles/af-ladder-read/files/read-califa-new-30000.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-tail -n +0 -f /home/af/journal.2017-01-13.5000.log | awk -v server_string="45.34.167.87 califa ActionFPS[local#30000]" -f /home/af/transform-journal-to-ladder.awk
+tail -n +0 -f /home/af/journal.2017-01-13.5000.log | awk -v server_string="califa.actionfps.com califa ActionFPS[local#30000]" -f /home/af/transform-journal-to-ladder.awk
--- roles/af-ladder-read/files/read-califa-new.sh
+++ roles/af-ladder-read/files/read-califa-new.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-tail -n +0 -f /home/af/journal.2017-01-13.5000.log | awk -v server_string="45.34.167.87 califa AssaultCube[califapublic]" -f /home/af/transform-journal-to-ladder.awk
+cat /home/af/journal.2017-01-13.5000.log | awk -v server_string="45.34.167.87 califa AssaultCube[califapublic]" -f /home/af/transform-journal-to-ladder.awk
+tail -n +0 -f /home/af/journal.2017-01-13.5000.log | awk -v server_string="califa.actionfps.com califa AssaultCube[califapublic]" -f /home/af/transform-journal-to-ladder.awk
